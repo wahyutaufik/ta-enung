8d2003e7dc4aad27db690d1c25d3acc9752da5bf
Wahyu Taufik <wahyutaufik37@gmail.com>
2014-12-13 15:22:07 +0700

    view product by category

--- modul/katalog.php
+++ modul/katalog.php
@@ -11,7 +11,17 @@ if (isset($_GET['page']) && !empty($_GET['page']))
 if (isset($_GET['perPage']) && !empty($_GET['perPage']))
     $dataPerPage = (int)$_GET['perPage'];
 
-$dataTable = getTableData($table, $page, $dataPerPage);
+
+$where = $_GET['kategori'];
+if (isset($where)) {
+    $sql = "SELECT * FROM $table WHERE id_kategori = $where";
+    $perKat = mysql_query($sql);
+    while ($datas = mysql_fetch_assoc($perKat)) {
+        $dataTable[] = $datas;
+    }
+} else {
+    $dataTable = getTableData($table, $page, $dataPerPage);
+}
 ?>
 <table  width="100%">
     <tbody>
@@ -22,16 +32,24 @@ $dataTable = getTableData($table, $page, $dataPerPage);
                 <a href="index.php?modul=produkDetail&id=<?php echo $data['id_barang'] ?>"><img src="gambar/gambar_barang/<?php echo $data['gambar'] ?>" width="100" alt=""></a>
                 <br>
                 <div class="harga">Rp. <?php echo $data['harga'] ?></div>
-                <a href="input.php?input=add&id=<?php echo $data['id_barang'] ?>"><strong>Beli</strong></a>
+                <a href="index.php?modul=beli&id=<?php echo $data['id_barang'] ?>"><strong>Beli</strong></a>
             </td>
             <td width="73%">
             	<a href="index.php?modul=produkDetail&id=<?php echo $data['id_barang'] ?>">
-            		<?php echo $data['nama_barang'] ?>
+            		<strong><?php echo $data['nama_barang'] ?></strong>
             	</a>
             	<p><?php echo substr($data['keterangan'], 0, 200) ?>...</p>
             	<p><strong>Stok:</strong> <?php echo $data['stok'] ?></p>
             	<strong>Kategori:</strong>
-            	<a href="index.php?modul=produk&kategori=<?php echo $data['id_kategori'] ?>">xxx</a>
+            	<a href="index.php?modul=produk&kategori=<?php echo $data['id_kategori'] ?>">
+                <?php  
+                $sql = "SELECT * FROM kategori WHERE id_kategori = $data[id_kategori]";
+                $datas = mysql_query($sql);
+                while ($kat = mysql_fetch_assoc($datas)) {
+                    echo $kat['nama_kategori'];
+                }
+                ?>
+                </a>
             	<br>
             	<br>
             	<br>
@@ -40,4 +58,6 @@ $dataTable = getTableData($table, $page, $dataPerPage);
         <?php endforeach ?>
     </tbody>
 </table>
+<?php if (!isset($_GET['kategori'])): ?>
 <?php showPagination($table, $dataPerPage);  ?>
+<?php endif ?>
